711a6893eeee1e78327b107810a9d733730cd382
harold alzate <haroldalzate99@gmail.com>
2023-05-02 18:05:39 -0500

    cambiando el estilo de las cards swiper

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,9 +1,16 @@
-let swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    freeMode: true,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
+const swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
     },
-});
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
