d6377335ae1d7596414a59d04d227771fd4b6abb
Diana Carolina Hernandez <dianacaro3552@gmail.com>
2023-05-06 22:35:25 -0500

    carrito básico finalizado

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -1,5 +1,7 @@
 let url = `https://pro-talento.up.railway.app/api/mindy/products`;
 
+let current_cart = []
+
 async function fecthApi() {
     try {
         let response = await fetch(url);
@@ -9,7 +11,7 @@ async function fecthApi() {
         let data_cart = JSON.parse(localStorage.getItem('cart')) ?? []
 
         //pasarlos a array de objetos
-        let prod_filtered = data_cart.map(each => {
+        current_cart = data_cart.map(each => {
             return response.products.find((obj) => obj._id === each)
         }).reduce((acc, product) => {
             let answer = acc
@@ -30,9 +32,9 @@ async function fecthApi() {
             return answer
         },[])
 
-        // mostrar carrito
+        // acciones en navbar y div principal para items carrito
         refresh_cart_in_navbar(data_cart.length);
-        printCards(prod_filtered);
+        printCards(current_cart);
     } catch (error) {
         console.log(error);
     }
@@ -40,7 +42,7 @@ async function fecthApi() {
 
 fecthApi();
 
-/* FUNCTION: refresca el numero de elementos en el carrito */
+/* FUNCTION: refresca el numero de elementos del carrito en el navbar */
 function refresh_cart_in_navbar(number_items) {
     let contador_unidades = document.getElementById("cart-count");
     contador_unidades.innerHTML = ``
@@ -69,12 +71,14 @@ function printCards(array) {
             <div class="col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4">
                 <h6 class="fw-bold text-body-secondary">${product.tipo}</h6>
                 <h6 class="fw-bold">${product.nombre}</h6>
+                <h6 class="fw-bold">$ ${product.precio}</h6>
             </div>
 
             <div class="col-2 col-sm-2 col-md-2 col-lg-2 col-xl-2">
                 <h6 class="fw-bold text-body-secondary">Cantidad</h6>
-                <input id="input${product._id}" class="form-control form-control-sm quantity"
-                name="quantity" type="number" min="0" value="${product.cant}" />
+                <input id="n${product._id}" class="form-control form-control-sm quantity"
+                name="quantity" type="number" min="0" value="${product.cant}"
+                oninput='updateValues("n${product._id}")' />
             </div>
 
             <div class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3 text-nowrap">
@@ -114,4 +118,18 @@ function calculate_subtotal(array) {
         subtotal = subtotal + (obj.precio * obj.cant)
         return [cant, subtotal]
     },[0,0]))
+}
+
+/* FUNCTION: actualiza valores en carrito */
+function updateValues (idInput) {
+    // tomando la nueva cantidad y actualizando carrito
+    let prod_to_update = current_cart.find((obj) => obj._id === idInput.substring(1))
+    prod_to_update.cant = Number(document.getElementById(idInput).value)
+    // actualizando subtotal del producto actualizado
+    document.getElementById("subt"+idInput.substring(1)).innerHTML = `$ ${prod_to_update.precio * prod_to_update.cant}`
+    // actualizando summary
+    let [cant, subtotal] = calculate_subtotal(current_cart);
+    printSummary(cant, subtotal)
+    // actujalizando navbar
+    refresh_cart_in_navbar(cant);
 }
